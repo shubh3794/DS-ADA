a5e379de4e2eefeee451b20687ac8fdf685961b1
shubham <shubh.aggarwal.37@gmail.com>
2015-12-03 20:49:19 +0530

    added bottom up approach to insertions to palindrome

--- Dynamic-Programming/MinInsToPalin.py
+++ Dynamic-Programming/MinInsToPalin.py
@@ -21,8 +21,23 @@ def Ins(a,lo,hi,m):
 
 def Insert():
     a = list(raw_input())
+    n = len(a)
     m = [[0]*len(a) for i in range(len(a))]
-    print 'top-bottom' Ins(a,0,len(a)-1,m)
-    
+    print 'top-bottom', Ins(a,0,len(a)-1,m)
+    m = [[0]*len(a) for i in range(len(a))]
+    for gap in range(1,n):
+        for i in range(n-gap):
+            j = i + gap
+            if i == j-1:
+                if a[i]==a[j]:
+                    m[i][j] = 0
+                else:
+                    m[i][j] = 1
+            elif a[i]==a[j]:
+                m[i][j] = m[i+1][j-1]
+            else:
+                m[i][j] = min(m[i+1][j],m[i][j-1])+1
+    print 'bottom up', m[0][n-1]
+                
     
 Insert()
