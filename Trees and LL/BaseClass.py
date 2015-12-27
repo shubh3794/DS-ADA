3b5bab2d303eefbd8c7d952b3c48ceac12647f91
shubham <shubh.aggarwal.37@gmail.com>
2015-12-27 17:20:27 +0530

    Added a base class for LL and Trees

---
+++ Trees and LL/BaseClass.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self.top = -1
+        self.__stack__ = []
+
+    def push(x):
+        self.top += 1
+        self.__stack__.append(x)
+        return self.__stack__[self.top]
+
+    def pop():
+        return self.__stack__.pop()
+
+
+class Node:
+    def __init__(self,data=None):
+        self.value = data
+        self.left = None
+        self.right = None
+        self.next = None
+        self.previous = None
+        self.random = None
+    def __str__(self):
+        return "Node is %s" % (self.value)
+    
