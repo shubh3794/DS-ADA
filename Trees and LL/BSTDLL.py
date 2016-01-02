e03178e94fa91156626d1fd2c8d7ef8e4fb6984a
shubham <shubh.aggarwal.37@gmail.com>
2016-01-03 00:43:37 +0530

    BST based ques

---
+++ Trees and LL/BSTDLL.py
@@ -0,0 +1,55 @@
+from BaseClass import BST
+from BTreeAndBST import inorder
+tree = BST()
+tree.ins(tree.root,6)
+tree.ins(tree.root,2)
+tree.ins(tree.root,4)
+tree.ins(tree.root,0)
+tree.ins(tree.root,3)
+tree.ins(tree.root,5)
+tree.ins(tree.root,1)
+tree.ins(tree.root,-1)
+
+tree.ins(tree.root,8)
+tree.ins(tree.root,7)
+tree.ins(tree.root,10)
+tree.ins(tree.root,9)
+tree.ins(tree.root,11)
+inorder(tree.root)
+print "\n"
+def BSTDLL(x,flag):
+    if x==None:
+        return
+    if x.left==None and x.right==None:
+        return x
+    l=None
+    r=None
+    if x.left:
+        l = BSTDLL(x.left,'r')
+    if x.right:
+        r = BSTDLL(x.right,'l')
+    if l:
+        l.next = x
+        x.prev = l
+    if r:
+        x.next = r
+        r.prev = x
+
+    if flag=='r':
+        if r==None:
+            r = x
+        while r.next!=None:
+            r = r.next
+        return r
+    if flag=='l':
+        if l==None:
+            l = x
+        while l.prev!=None:
+            l = l.prev
+        return l
+m = BSTDLL(tree.root,'l')
+
+curr = m
+while curr!=None:
+    print curr.value
+    curr = curr.next
--- Trees and LL/BaseClass.py
+++ Trees and LL/BaseClass.py
@@ -1,7 +1,7 @@
 class Stack:
     def __init__(self):
         self.top = -1
-        self.__stack__ = []
+        self.__stack__ = [None]
 
     def push(x):
         self.top += 1
@@ -11,6 +11,32 @@ class Stack:
     def pop():
         return self.__stack__.pop()
 
+class DynamicStack:
+    def __init__(self):
+        self.top = 0
+        self.__stack__ = [None]*1
+        
+    def push(self,value):
+        if len(self.__stack__)==self.top:
+            self.resize(2*len(self.__stack__))
+
+        (self.__stack__)[self.top]=value
+        self.top += 1
+        return (self.__stack__)[self.top-1]
+    
+    def resize(self,newCap):
+        lst = [None]*(newCap)
+        for i in range(newCap/2):
+            lst[i] = (self.__stack__)[i]
+        self.__stack__=lst
+
+    def pop(self):
+        self.top -= 1
+        temp = self.__stack__[self.top]
+        self.__stack__[self.top]=None
+        if self.top>0 and self.top == len(self.__stack__)/4:
+            self.resize(len(self.__stack__)/2)
+        return temp
 
 class Node:
     def __init__(self,data=None):
@@ -18,7 +44,7 @@ class Node:
         self.left = None
         self.right = None
         self.next = None
-        self.previous = None
+        self.prev = None
         self.random = None
     def __str__(self):
         return "Node is %s" % (self.value)
@@ -101,5 +127,3 @@ class Queue:
             return True
         else:
             return False
-        
-       
